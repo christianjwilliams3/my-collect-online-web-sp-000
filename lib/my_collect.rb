c8d2cf74cf9296a7fe99b07f1bbaf6a0c4ab6eb5
christian williams <christian.williamsj@gmail.com>
2019-09-07 20:44:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(collection)
-  i = 0 
-      new.collection = []
-    while i < collection.length 
-      new_collection << yield(collection[i])
-    i += 1 
-    end
-  new.collection
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
-  
 
