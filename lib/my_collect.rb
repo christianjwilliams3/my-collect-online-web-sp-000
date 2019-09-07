374220cd0e266cefd4f978dec166183f2891b53c
christian williams <christian.williamsj@gmail.com>
2019-09-07 20:40:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,9 @@ def my_collect(collection)
   i = 0 
     new.collection =[]
     while i > collection.length 
-    new_collection << yield(collection)
+    new_collection << yield(collection[i])
+end
+new.collection
 end
   
 
